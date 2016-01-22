649506dd7f9658a6ed6ba011cbdb0ac28e4c804c
Титаевский Сергей <Titaevskiy.S@gmail.com>
2016-01-22 15:45:03 +0300

    Format restore_G2

--- restore_G2.m
+++ restore_G2.m
@@ -1,8 +1,4 @@
 function [ G2 ] = restore_G2( G1, S )
-%RESTORE_G2 Summary of this function goes here
-%   Detailed explanation goes here
-
-G2 = (G1 - S(1,1)) ./ (S(1,2) .* S(2,1) - S(1,1) .* S(2,2) + G1 .* S(2,2));
-
+    G2 = (G1 - S(1,1)) ./ (S(1,2) .* S(2,1) - S(1,1) .* S(2,2) + G1 .* S(2,2));
 end
 
