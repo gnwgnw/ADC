89bb57779ba9404fca7dbbc61754f46cf4402d43
Титаевский Сергей <Titaevskiy.S@gmail.com>
2016-01-23 18:59:20 +0300

    Add the plot view class

---
+++ View_plot.m
@@ -0,0 +1,43 @@
+classdef View_plot < View_base
+    properties (Access = private)
+        x_prop_name;
+        y_prop_name;
+        hline;
+    end
+
+    methods
+        function obj = View_plot(hobj, model, x_prop_name, y_prop_name)
+            obj@View_base(hobj, model);
+
+            obj.x_prop_name = x_prop_name;
+            obj.y_prop_name = y_prop_name;
+
+            obj.init_plot();
+            obj.init_listeners();
+        end
+
+        function enable(~, ~)
+            % nothing to do
+        end
+    end
+
+    methods (Access = private)
+        function init_plot(obj)
+            axes(obj.hobj);
+            obj.hline = plot(0,0);
+        end
+
+        function init_listeners(obj)
+            addlistener(obj.model, obj.x_prop_name, 'PostSet', @obj.on_x_change);
+            addlistener(obj.model, obj.y_prop_name, 'PostSet', @obj.on_y_change);
+        end
+
+        function on_x_change(obj, ~, ~)
+            obj.hline.XData = obj.model.(obj.x_prop_name);
+        end
+
+        function on_y_change(obj, ~, ~)
+            obj.hline.YData = obj.model.(obj.y_prop_name);
+        end
+    end
+end
