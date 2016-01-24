c60b0d87806df8ec9d59532b85925be191b26cbb
Титаевский Сергей <Titaevskiy.S@gmail.com>
2016-01-25 00:07:00 +0300

    Edit text control

---
+++ Control_edit.m
@@ -0,0 +1,41 @@
+classdef Control_edit < Control_base
+    properties
+        listen_prop_name;
+    end
+
+    methods
+        function obj = Control_edit(hobj, model, prop_name)
+            obj@Control_base(hobj, model);
+
+            obj.listen_prop_name = prop_name;
+
+            obj.hobj.String = num2str(obj.model.(obj.listen_prop_name));
+
+            obj.init_listeners();
+            obj.init_callbacks();
+        end
+
+        function enable(obj, status)
+            obj.hobj.Enable = status;
+        end
+    end
+
+    methods (Access = private)
+        function init_listeners(obj)
+            addlistener(obj.model, obj.listen_prop_name, 'PostSet', @obj.on_change);
+        end
+
+        function on_change(obj, ~, ~)
+            obj.hobj.String = obj.model.(obj.listen_prop_name);
+        end
+
+        function init_callbacks(obj)
+            obj.hobj.Callback = @obj.callback;
+        end
+
+        function callback(obj,~,~)
+            val = str2double(obj.hobj.String);
+            obj.model.(obj.listen_prop_name) = val;
+        end
+    end
+end
